abf0e3b1cd9dff3fc92d9adb5783e029fc254f11
thegood.eth <172343141+thegoodeth12@users.noreply.github.com>
2025-07-20 02:16:18 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,9 +1,24 @@
-import { mount } from 'svelte'
-import './app.css'
-import App from './App.svelte'
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
 
-const app = mount(App, {
-  target: document.getElementById('app')!,
-})
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
 
-export default app
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
